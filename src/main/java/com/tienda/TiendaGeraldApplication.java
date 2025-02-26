a48a1b2d77d7bf87741b74037111a316fa49b46a
GeraldAndrey <gmorales1399@gmail.com>
2025-02-26 16:51:12 -0600

    Trabajo Clase #7

---
+++ src/main/java/com/tienda/TiendaGeraldApplication.java
@@ -0,0 +1,13 @@
+package com.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaGeraldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaGeraldApplication.class, args);
+	}
+
+}
